0c3d3e2ecac164dcc1aeb1a2da8125dcb3892b5f
Charlie <769634353@qq.com>
2017-11-16 08:33:14 +0800

    Huffman

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a = "Hello, World!";
+    cin >> a;
+    if(!cin){
+        cout<<a<<endl;
+        cout<<a<<endl;
+    }
+    cout<< "what"<<endl;
+    return 0;
+}
